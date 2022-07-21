f0c14b3d37915a23b1f0f633ca6cc634c5f01738
hasibulislam999 <hasib143sl@gmail.com>
2022-07-21 10:31:45 +0600

    default error handler integrated

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+// application route
 app.get('/', (req, res) => {
     res.send('express app connected')
+})
+
+// default error handler
+app.use((err, req, res, next) => {
+    if (err.headersSent) {
+        return next(err);
+    } else {
+        res.status(500).json({ error: err });
+    }
 })
 
 app.listen(3000, () => {
