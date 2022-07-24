4ddd7c12559c719062168362f1928e5eec8a4a17
hasibulislam999 <hasib143sl@gmail.com>
2022-07-24 22:33:51 +0600

    adding user in todo for relational db idea

--- schemas/todo.js
+++ schemas/todo.js
@@ -1,46 +1,50 @@
 const mongoose = require("mongoose");
 
 const todoSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: String,
-  status: {
-    type: String,
-    enum: ["active", "inactive"],
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    status: {
+        type: String,
+        enum: ["active", "inactive"],
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    }
 });
 
 // static methods
 todoSchema.statics = {
-  findByJS: function () {
-    return this.find({ title: /js/i });
-  }
+    findByJS: function () {
+        return this.find({ title: /js/i });
+    }
 }
 
 // instance methods
 todoSchema.methods = {
-  findActive: function () {
-    return mongoose.model("Todo").find({ status: "active" });
-  },
-  findInActive: function () {
-    return mongoose.model("Todo").find({ status: "inactive" });
-  },
-  findActiveCallback: function (cb) {
-    return mongoose.model("Todo").find({ status: "active" }, cb);
-  }
+    findActive: function () {
+        return mongoose.model("Todo").find({ status: "active" });
+    },
+    findInActive: function () {
+        return mongoose.model("Todo").find({ status: "inactive" });
+    },
+    findActiveCallback: function (cb) {
+        return mongoose.model("Todo").find({ status: "active" }, cb);
+    }
 }
 
 // query helper
 todoSchema.query = {
-  byLang: function (lang) {
-    return this.find({ title: new RegExp(lang, "i") });
-  }
+    byLang: function (lang) {
+        return this.find({ title: new RegExp(lang, "i") });
+    }
 }
 
 module.exports = todoSchema;
