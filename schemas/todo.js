54167ad2e917352fe1924555ee7508b920cd19f4
hasibulislam999 <hasib143sl@gmail.com>
2022-07-24 22:03:08 +0600

    enhance todo schema

---
+++ schemas/todo.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// static methods
+todoSchema.statics = {
+  findByJS: function () {
+    return this.find({ title: /js/i });
+  }
+}
+
+// instance methods
+todoSchema.methods = {
+  findActive: function () {
+    return mongoose.model("Todo").find({ status: "active" });
+  },
+  findInActive: function () {
+    return mongoose.model("Todo").find({ status: "inactive" });
+  },
+  findActiveCallback: function (cb) {
+    return mongoose.model("Todo").find({ status: "active" }, cb);
+  }
+}
+
+// query helper
+todoSchema.query = {
+  byLang: function (lang) {
+    return this.find({ title: new RegExp(lang, "i") });
+  }
+}
+
+module.exports = todoSchema;
